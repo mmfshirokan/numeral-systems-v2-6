7b440761c1b08eaa223b9fd4c4c60a029b41bd94
Watсher <Wather@DESKTOP-VL24RQ1>
2022-09-28 18:50:14 +0300

    All metods implimented/

--- NumeralSystems/Converter.cs
+++ NumeralSystems/Converter.cs
@@ -18,7 +18,10 @@ namespace NumeralSystems
         /// contains invalid symbols (non-octal alphabetic characters).
         /// Valid octal alphabetic characters: 0,1,2,3,4,5,6,7.
         /// </exception>
-        public static int ParsePositiveFromOctal(this string source) => throw new NotImplementedException();
+        public static int ParsePositiveFromOctal(this string source)
+        {
+            return ParsePositiveByRadix(source, 8);
+        }
 
         /// <summary>
         /// Converts the string representation of a positive number in the decimal numeral system to its 32-bit signed integer equivalent.
@@ -31,7 +34,10 @@ namespace NumeralSystems
         /// contains invalid symbols (non-decimal alphabetic characters).
         /// Valid decimal alphabetic characters: 0,1,2,3,4,5,6,7,8,9.
         /// </exception>
-        public static int ParsePositiveFromDecimal(this string source) => throw new NotImplementedException();
+        public static int ParsePositiveFromDecimal(this string source)
+        {
+            return ParsePositiveByRadix(source, 10);
+        }
 
         /// <summary>
         /// Converts the string representation of a positive number in the hex numeral system to its 32-bit signed integer equivalent.
@@ -44,7 +50,10 @@ namespace NumeralSystems
         /// contains invalid symbols (non-hex alphabetic characters).
         /// Valid hex alphabetic characters: 0,1,2,3,4,5,6,7,8,9,A(or a),B(or b),C(or c),D(or d),E(or e),F(or f).
         /// </exception>
-        public static int ParsePositiveFromHex(this string source) => throw new NotImplementedException();
+        public static int ParsePositiveFromHex(this string source)
+        {
+            return ParsePositiveByRadix(source, 16);
+        }
 
         /// <summary>
         /// Converts the string representation of a positive number in the octal, decimal or hex numeral system to its 32-bit signed integer equivalent.
@@ -59,7 +68,15 @@ namespace NumeralSystems
         /// -or-
         /// the radix is not equal 8, 10 or 16.
         /// </exception>
-        public static int ParsePositiveByRadix(this string source, int radix) => throw new NotImplementedException();
+        public static int ParsePositiveByRadix(this string source, int radix)
+        {
+          if (ParseByRadix(source, radix) < 0)
+            {
+                throw new ArgumentException("source string presents a negative number");
+            }
+
+          return ParseByRadix(source, radix);
+        }
 
         /// <summary>
         /// Converts the string representation of a signed number in the octal, decimal or hex numeral system to its 32-bit signed integer equivalent.
@@ -72,7 +89,46 @@ namespace NumeralSystems
         /// -or-
         /// the radix is not equal 8, 10 or 16.
         /// </exception>
-        public static int ParseByRadix(this string source, int radix) => throw new NotImplementedException();
+        public static int ParseByRadix(this string source, int radix)
+        {
+            if (radix != 8 & radix != 10 & radix != 16)
+            {
+                throw new ArgumentException("the radix is not equal 8, 10 or 16");
+            }
+
+            double CharToNum(char a) => a switch
+            {
+                '0' => 0, '1' => 1, '2' => 2,
+                '3' => 3, '4' => 4, '5' => 5,
+                '6' => 6, '7' => 7, '8' => 8,
+                '9' => 9, 'A' => 10, 'B' => 11,
+                'C' => 12, 'D' => 13,
+                'E' => 14, 'F' => 15,
+                'a' => 10, 'b' => 11,
+                'c' => 12, 'd' => 13,
+                'e' => 14, 'f' => 15,
+                '-' =>0,
+                _ => throw new ArgumentException("source contains invalid for given numeral system symbols"),
+            };
+            uint result = 0;
+            for (int i = 0; i < source.Length; i++)
+            {
+                double tempElement = CharToNum(source[source.Length - 1 - i]);
+                result += (uint)(tempElement * Math.Pow(radix, i));
+
+                if ((radix == 8 & tempElement > 7) | (radix == 10 & tempElement > 9))
+                {
+                    throw new ArgumentException("source contains invalid for given numeral system symbols");
+                }
+            }
+
+            if (source[0] == '-')
+            {
+                return -((int)result);
+            }
+
+            return (int)result;
+        }
 
         /// <summary>
         /// Converts the string representation of a positive number in the octal numeral system to its 32-bit signed integer equivalent.
@@ -81,7 +137,10 @@ namespace NumeralSystems
         /// <param name="source">The string representation of a positive number in the octal numeral system.</param>
         /// <param name="value">A positive decimal value.</param>
         /// <returns>true if s was converted successfully; otherwise, false.</returns>
-        public static bool TryParsePositiveFromOctal(this string source, out int value) => throw new NotImplementedException();
+        public static bool TryParsePositiveFromOctal(this string source, out int value)
+        {
+            return TryParsePositiveByRadix(source, 8, out value);
+        }
 
         /// <summary>
         /// Converts the string representation of a positive number in the decimal numeral system to its 32-bit signed integer equivalent.
@@ -91,7 +150,10 @@ namespace NumeralSystems
         /// <returns>A positive decimal value.</returns>
         /// <param name="value">A positive decimal value.</param>
         /// <returns>true if s was converted successfully; otherwise, false.</returns>
-        public static bool TryParsePositiveFromDecimal(this string source, out int value) => throw new NotImplementedException();
+        public static bool TryParsePositiveFromDecimal(this string source, out int value)
+        {
+            return TryParsePositiveByRadix(source, 10, out value);
+        }
 
         /// <summary>
         /// Converts the string representation of a positive number in the hex numeral system to its 32-bit signed integer equivalent.
@@ -101,7 +163,10 @@ namespace NumeralSystems
         /// <returns>A positive decimal value.</returns>
         /// <param name="value">A positive decimal value.</param>
         /// <returns>true if s was converted successfully; otherwise, false.</returns>
-        public static bool TryParsePositiveFromHex(this string source, out int value) => throw new NotImplementedException();
+        public static bool TryParsePositiveFromHex(this string source, out int value)
+        {
+            return TryParsePositiveByRadix(source, 16, out value);
+        }
 
         /// <summary>
         /// Converts the string representation of a positive number in the octal, decimal or hex numeral system to its 32-bit signed integer equivalent.
@@ -113,7 +178,24 @@ namespace NumeralSystems
         /// <param name="value">A positive decimal value.</param>
         /// <returns>true if s was converted successfully; otherwise, false.</returns>
         /// <exception cref="ArgumentException">Thrown the radix is not equal 8, 10 or 16.</exception>
-        public static bool TryParsePositiveByRadix(this string source, int radix, out int value) => throw new NotImplementedException();
+        public static bool TryParsePositiveByRadix(this string source, int radix, out int value)
+        {
+            if (radix != 8 & radix != 10 & radix != 16)
+            {
+                throw new ArgumentException("the radix is not equal 8, 10 or 16");
+            }
+
+            value = 0;
+            try
+            {
+                value = ParsePositiveByRadix(source, radix);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
 
         /// <summary>
         /// Converts the string representation of a signed number in the octal, decimal or hex numeral system to its 32-bit signed integer equivalent.
@@ -125,6 +207,23 @@ namespace NumeralSystems
         /// <param name="value">A positive decimal value.</param>
         /// <returns>true if s was converted successfully; otherwise, false.</returns>
         /// <exception cref="ArgumentException">Thrown the radix is not equal 8, 10 or 16.</exception>
-        public static bool TryParseByRadix(this string source, int radix, out int value) => throw new NotImplementedException();
+        public static bool TryParseByRadix(this string source, int radix, out int value)
+        {
+            if (radix != 8 & radix != 10 & radix != 16)
+            {
+                throw new ArgumentException("the radix is not equal 8, 10 or 16");
+            }
+
+            value = 0;
+            try
+            {
+                value = ParseByRadix(source, radix);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
